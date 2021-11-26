d61ad1b4b949715264c9a0b0bd3b654be4845bb4
Raul Figueroa <rfigueroa17@hotmail.com>
2021-11-26 16:42:48 -0700

    add test files. display number of cars at homepage

--- src/test/items.js
+++ src/test/items.js
@@ -1,18 +1,6 @@
-/* eslint-disable no-unused-vars, no-restricted-globals */
-import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './style.css';
-import Api from './app/api';
-import Likes from './app/likes';
+/* eslint-disable no-undef, no-restricted-globals */
+const countCars = () => 5;
 
-const api = new Api();
-const likes = new Likes();
-function importAll(r) {
-  return r.keys().map(r);
-}
-const images = importAll(require.context('./assets/images', false, /\.(png|jpe?g|svg)$/));
-const itemsContainer = document.querySelector('#itemsContainer');
-const carNumber = document.querySelector('#carNumber');
 const addItem = (carName) => {
   const itemContainer = document.createElement('div');
   itemContainer.classList.add('col-sm-12', 'col-md-6', 'col-lg-4', 'row', 'justify-content-center');
@@ -65,22 +53,4 @@ const addItem = (carName) => {
   itemContainer.appendChild(reservationsButton);
 };
 
-const countCars = () => itemsContainer.childElementCount;
-
-api.getData()
-  // .then((response) => response)
-  .then((data) => {
-    data.Results.forEach((e) => {
-      addItem(e.Model_Name);
-    });
-    carNumber.innerText = `Cars(${countCars()})`;
-    likes.getLikes()
-      .then((response) => {
-        response.forEach((l) => {
-          const updateLikes = document.getElementById(`${l.item_id}likes`);
-          if (updateLikes) {
-            updateLikes.innerText = `${l.likes} likes`;
-          }
-        });
-      });
-  });
+export { countCars, addItem };
